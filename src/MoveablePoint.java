64b117bdeca27de3edb4e7a422eefa5b880d7b1c
longnhat120320 <nhat120320@gmail.com>
2024-04-11 15:00:18 +0700

    Lan3

---
+++ src/MoveablePoint.java
@@ -0,0 +1,32 @@
+public class MoveablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint() {
+    }
+
+
+    public MoveablePoint move() {
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+}
